0ba86494cc6f6328009cd2c5f52e678e0fd39c91
aattanasio91 <agustin.attanasio@gmail.com>
2021-04-07 21:13:37 -0300

    Se realizaron correcciones sobre metodos.

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -5,26 +5,17 @@ class CentroDistribucion(val ciudad:Ciudad){
     val vendedores = mutableListOf<Vendedor>()
 
     fun agregarVendedor(vendedor:Vendedor){
-        if(!vendedores.contains(vendedor)){
-            vendedores.add(vendedor)
-        }else{
+        if(vendedores.contains(vendedor)) {
             throw Exception("No se puede agregar dos veces el mismo vendedor")
         }
+        vendedores.add(vendedor)
     }
 
-    fun vendedorEstrella() : Vendedor? {
-        return vendedores.maxBy { it.puntajeCertificaciones() }
-    }
+    fun vendedorEstrella() : Vendedor? = vendedores.maxBy { it.puntajeCertificaciones() }
 
-    fun puedeCubrirCiudad(ciudad:Ciudad) : Boolean{
-        return vendedores.any { it.puedeTrabajarEn(ciudad) }
-    }
+    fun puedeCubrirCiudad(ciudad:Ciudad) : Boolean = vendedores.any { it.puedeTrabajarEn(ciudad) }
 
-    fun vendedoresGenericos() : List<Vendedor>{
-        return vendedores.filter { it.otrasCertificaciones() >= 1 }
-    }
+    fun vendedoresGenericos() : List<Vendedor> = vendedores.filter { it.otrasCertificaciones() >= 1 }
 
-    fun esRobusto() : Boolean {
-        return vendedores.filter { it.esFirme() }.count() >= 3
-    }
+    fun esRobusto() : Boolean = vendedores.count{ it.esFirme() } >= 3
 }
